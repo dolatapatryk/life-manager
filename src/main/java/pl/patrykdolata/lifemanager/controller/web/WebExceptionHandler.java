b81112cba76f52d5ba53e93eb0e0c9fa5db55e0b
dolatapatryk1 <dolata.patryk1@gmail.com>
2021-01-12 21:45:06 +0100

    added WebExceptionHandler to handle authentication and register exceptions

---
+++ src/main/java/pl/patrykdolata/lifemanager/controller/web/WebExceptionHandler.java
@@ -0,0 +1,33 @@
+package pl.patrykdolata.lifemanager.controller.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import pl.patrykdolata.lifemanager.exceptions.WebException;
+import pl.patrykdolata.lifemanager.model.ErrorResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@ControllerAdvice
+public class WebExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(AuthenticationException.class)
+    public final ResponseEntity<ErrorResponse> handleAuthenticationExceptions(Exception ex, HttpServletRequest request) {
+        return new ResponseEntity<>(createErrorResponse(ex, HttpStatus.UNAUTHORIZED.value(), request),
+                HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(WebException.class)
+    public final ResponseEntity<ErrorResponse> handleWebExceptions(WebException ex, HttpServletRequest request) {
+        return new ResponseEntity<>(createErrorResponse(ex, ex.getStatusCode().value(), request),
+                ex.getStatusCode());
+    }
+
+    private ErrorResponse createErrorResponse(Exception ex, int status, HttpServletRequest request) {
+        return new ErrorResponse(ex.getMessage(), status, new Date().getTime(), request.getRequestURI());
+    }
+}

02f7c5a5fa0e9ed6053d0f59f86703788a573907
dolatapatryk1 <dolata.patryk1@gmail.com>
2021-01-13 22:56:51 +0100

    implemented logic of creating account for user

--- src/main/java/pl/patrykdolata/lifemanager/service/impl/AccountServiceImpl.java
+++ src/main/java/pl/patrykdolata/lifemanager/service/impl/AccountServiceImpl.java
@@ -3,6 +3,8 @@ package pl.patrykdolata.lifemanager.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import pl.patrykdolata.lifemanager.domain.AccountEntity;
+import pl.patrykdolata.lifemanager.mapper.AccountMapper;
 import pl.patrykdolata.lifemanager.model.NewAccount;
 import pl.patrykdolata.lifemanager.repository.AccountRepository;
 import pl.patrykdolata.lifemanager.service.AccountService;
@@ -13,13 +15,18 @@ public class AccountServiceImpl implements AccountService {
     private final Logger log = LoggerFactory.getLogger(AccountServiceImpl.class);
 
     private final AccountRepository accountRepository;
+    private final AccountMapper accountMapper;
 
-    public AccountServiceImpl(AccountRepository accountRepository) {
+    public AccountServiceImpl(AccountRepository accountRepository, AccountMapper accountMapper) {
         this.accountRepository = accountRepository;
+        this.accountMapper = accountMapper;
     }
 
     @Override
     public Long create(NewAccount account) {
-        return null;
+        log.debug("Create new account with name: {}", account.getName());
+        AccountEntity newAccount = accountRepository.save(accountMapper.toEntity(account));
+
+        return newAccount.getId();
     }
 }
